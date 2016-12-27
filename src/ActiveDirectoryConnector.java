997cb47e97b7996269aa469d6a13286fd0dff6b0
jsung5381 <jsung5381@naver.com>
2016-12-27 18:10:59 +0900

    Implemented tests in the main method to give guide lines to the program.

---
+++ src/ActiveDirectoryConnector.java
@@ -0,0 +1,7 @@
+/**
+ * Created by jsh3571 on 27/12/2016.
+ */
+
+public class ActiveDirectoryConnector {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@
 
 public class Main {
     public static void main(String[] args) {
+        String host = "192.168.100.173";
+        String port = "389";
+        String username = "admin";
+        String password = "privacy!@34";
+        String baseDn = "cn=users,dc=comtrue,dc=com";
+        String filter = "company=";
 
+        ActiveDirectoryConnector dao =
+                new ActiveDirectoryConnector(
+                        host, port, username, password, baseDn, filter);
+
+        System.out.println(dao.getUser("test"));
+        dao.close();
     }
 }
