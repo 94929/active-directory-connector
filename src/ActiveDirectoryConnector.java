9d2f5704b75374d2f754cd2a38d11a1b3816fa49
jsung5381 <jsung5381@naver.com>
2016-12-27 19:09:54 +0900

    Implemented fields and parameters of ActiveDirectoryConnector class and constructor.

--- src/ActiveDirectoryConnector.java
+++ src/ActiveDirectoryConnector.java
@@ -1,7 +1,20 @@
+import javax.naming.directory.DirContext;
+
 /**
  * Created by jsh3571 on 27/12/2016.
  */
 
 public class ActiveDirectoryConnector {
+    private DirContext ctx;
+    private String baseDn;
+    private String filter;
 
+    public ActiveDirectoryConnector(String host, String port,
+                                    String username, String password,
+                                    String baseDn, String filter) {
+        this.baseDn = baseDn;
+        this.filter = filter;
+
+
+    }
 }
